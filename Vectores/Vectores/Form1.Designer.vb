73dc74f7b09052abb9ebc459407ddf182e020622
Facundototo <81201231+Facundototo@users.noreply.github.com>
2022-11-21 20:50:50 -0300

    Add files via upload

---
+++ Vectores/Vectores/Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ListViewItem5 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem("")
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.NumericUpDown5 = New System.Windows.Forms.NumericUpDown()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Refresh = New System.Windows.Forms.Button()
+        Me.NumericUpDown6 = New System.Windows.Forms.NumericUpDown()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(119, 12)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 0
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(66, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Division:"
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(119, 38)
+        Me.NumericUpDown2.Maximum = New Decimal(New Integer() {999, 0, 0, 0})
+        Me.NumericUpDown2.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown2.TabIndex = 2
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(4, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Cantidad de alumnos:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(119, 130)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Visible = False
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Location = New System.Drawing.Point(119, 256)
+        Me.NumericUpDown3.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown3.TabIndex = 5
+        Me.NumericUpDown3.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown3.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(4, 133)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(94, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Nombre y apellido:"
+        Me.Label3.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(-1, 258)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(114, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Nota de la evaluacion:"
+        Me.Label4.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(164, 282)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Guardar"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(4, 161)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(96, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Cantidad de notas:"
+        Me.Label5.Visible = False
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.Location = New System.Drawing.Point(119, 161)
+        Me.NumericUpDown4.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown4.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown4.TabIndex = 9
+        Me.NumericUpDown4.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown4.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(151, 196)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(88, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Crear Almuno"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(151, 64)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(88, 23)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "Crear Division"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1})
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem5})
+        Me.ListView1.Location = New System.Drawing.Point(280, 14)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(274, 291)
+        Me.ListView1.TabIndex = 13
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'NumericUpDown5
+        '
+        Me.NumericUpDown5.Location = New System.Drawing.Point(119, 104)
+        Me.NumericUpDown5.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown5.Name = "NumericUpDown5"
+        Me.NumericUpDown5.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown5.TabIndex = 14
+        Me.NumericUpDown5.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown5.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(7, 104)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(83, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Alumno numero:"
+        Me.Label6.Visible = False
+        '
+        'Refresh
+        '
+        Me.Refresh.Location = New System.Drawing.Point(369, 311)
+        Me.Refresh.Name = "Refresh"
+        Me.Refresh.Size = New System.Drawing.Size(75, 23)
+        Me.Refresh.TabIndex = 16
+        Me.Refresh.Text = "Refresh"
+        Me.Refresh.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown6
+        '
+        Me.NumericUpDown6.Location = New System.Drawing.Point(119, 230)
+        Me.NumericUpDown6.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown6.Name = "NumericUpDown6"
+        Me.NumericUpDown6.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown6.TabIndex = 17
+        Me.NumericUpDown6.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(42, 232)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(71, 13)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Nota numero:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(590, 390)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.NumericUpDown6)
+        Me.Controls.Add(Me.Refresh)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.NumericUpDown5)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.NumericUpDown4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown2 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents NumericUpDown3 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown4 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents NumericUpDown5 As NumericUpDown
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Refresh As Button
+    Friend WithEvents NumericUpDown6 As NumericUpDown
+    Friend WithEvents Label7 As Label
+End Class
---
+++ Vectores/Vectores/Form1.vb
@@ -0,0 +1,141 @@
+﻿Public Class Form1
+    Dim division As Integer = 0
+    Dim notas As Integer = 0
+    Dim alumno As Integer = 0
+    Dim numeronota As Integer = 0
+    Dim maximo As Integer = 0
+    Dim maximo2 As Integer = 0
+    Dim maximo3 As Integer = 0
+
+    Dim notasguardadas As Integer = 0
+    Dim cursoscreados As Integer = 0
+    Dim alumnoscreados As Integer = 0
+
+    Dim SeUso(10) As Boolean
+    Dim SeUso2(10, 1) As Boolean
+    Dim SeUso3(10, 255, 255) As Boolean
+    Dim div(10) As Integer
+    Dim Nombre(10, 1) As String
+    Dim Nota(10, 255, 255) As Integer
+    Dim cantnotas(10, 100) As Integer
+
+    Private Sub NumericUpDown1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NumericUpDown1.ValueChanged
+        division = NumericUpDown1.Value
+
+        If SeUso(division) = False Then
+            Button3.Enabled = True
+        Else
+            Button3.Enabled = False
+        End If
+        NumericUpDown5.Maximum = div(division)
+    End Sub
+    Private Sub NumericUpDown5_ValueChanged(sender As Object, e As EventArgs)
+        alumno = NumericUpDown5.Value
+        If SeUso2(division, alumno) = False Then
+            Button2.Enabled = True
+        Else
+            Button2.Enabled = False
+        End If
+        NumericUpDown6.Maximum = cantnotas(division, alumno)
+    End Sub
+    Private Sub NumericUpDown6_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown6.ValueChanged
+        numeronota = NumericUpDown6.Value
+        If SeUso3(division, alumno, numeronota) = True Then
+            Button1.Text = "Cambiar"
+        Else
+            Button1.Text = "Guardar"
+        End If
+
+    End Sub
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        division = NumericUpDown1.Value
+        Label3.Visible = True
+        Label5.Visible = True
+        Label6.Visible = True
+        NumericUpDown4.Visible = True
+        NumericUpDown5.Visible = True
+        TextBox1.Visible = True
+        Button2.Visible = True
+        div(division) = NumericUpDown2.Value
+        cursoscreados = cursoscreados + 1
+
+        SeUso(NumericUpDown1.Value) = True
+
+        If maximo = 0 Or div(division) > maximo Then
+            ReDim Preserve Nombre(10, div(division))
+            ReDim Preserve SeUso2(10, div(division))
+            maximo = div(division)
+        End If
+
+        If SeUso(division) = False Then
+            Button3.Enabled = True
+        Else
+            Button3.Enabled = False
+        End If
+        NumericUpDown5.Maximum = div(division)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        alumno = NumericUpDown5.Value
+        Nombre(division, alumno) = TextBox1.Text
+        alumnoscreados = alumnoscreados + 1
+
+        If maximo3 = 0 Or cantnotas(division, alumno) > maximo3 Then
+            ReDim Preserve cantnotas(10, alumno)
+            maximo3 = cantnotas(division, alumno)
+        End If
+
+        cantnotas(division, alumno) = NumericUpDown4.Value
+
+        Label4.Visible = True
+        NumericUpDown3.Visible = True
+        Button1.Visible = True
+        SeUso2(division, alumno) = True
+
+        If SeUso2(division, alumno) = False Then
+            Button2.Enabled = True
+        Else
+            Button2.Enabled = False
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Nota(division, alumno, numeronota) = NumericUpDown3.Value
+
+        SeUso3(division, alumno, numeronota) = True
+
+        If Button1.Text = "Guardar" Then
+            notasguardadas = notasguardadas + 1
+        End If
+
+        If SeUso3(division, alumno, numeronota) = True Then
+            Button1.Text = "Cambiar"
+        Else
+            Button1.Text = "Guardar"
+        End If
+    End Sub
+    Private Sub Refresh_Click(sender As Object, e As EventArgs) Handles Refresh.Click
+        ListView1.Items.Clear()
+        For i As Integer = 1 To cursoscreados
+            For j As Integer = 1 To alumnoscreados
+                Dim suma_notas, prom As Double
+                Dim NoEv As Boolean = False
+                For k As Integer = 1 To cantnotas(i, j)
+                    If Nota(i, j, k) = 0 Then
+                        NoEv = True
+                    Else
+                        suma_notas = suma_notas + Nota(i, j, k)
+                    End If
+                Next
+                prom = suma_notas / cantnotas(i, j)
+                If NoEv = True Then
+                    ListView1.Items.Add(Nombre(i, j) + " No esta evaluado")
+                ElseIf prom >= 6 Then
+                    ListView1.Items.Add(Nombre(i, j) + " " & prom & " Aprobo")
+                Else
+                    ListView1.Items.Add(Nombre(i, j) + " " & prom & " Desaprobo")
+                End If
+            Next
+        Next
+    End Sub
+
+End Class
